2feca986b86046b6a74650bf34ee1b70b4ac53b7
Linh Linh <52542267+halinh1512@users.noreply.github.com>
2019-07-22 10:51:17 +0200

    Update SECOM_Script.R

--- SECOM_Script.R
+++ SECOM_Script.R
@@ -1,7 +1,6 @@
 # Set up work env and load libraries
 Sys.setenv(language="en")
 
-
 if(!require("pacman")) install.packages("pacman")
 library("pacman")
 p_load("ggplot2")
@@ -65,7 +64,6 @@ nrow(secom.test[secom.test$Status==-1,])/nrow(secom.test[secom.test$Status==1,])
 ## 2. Features with significant Numbers of NA
 # function to calculate frequency & percentage of NA
 #== na.count [Input: data frame df, dim = 1 (row) or 2 (column) / Output: sum of NA]
-# !!! Well documented - perfect!
 secom.na.count <- function(df,dim)                 
 {
   apply(df,dim,function(x) sum(is.na(x)))
@@ -82,7 +80,6 @@ secom.NA.per.col.count<-secom.na.count(secom.data.train,2)
 secom.NA.per.col.perc<-secom.na.perc(secom.data.train,2)
 secom.NA.col.table<-data.frame(Feature = 1:ncol(secom.data.train), Percentage = round(secom.NA.per.col.perc,3), Frequency = secom.NA.per.col.count)
 
-# !!! parameter an be defined at beginning of script, use more appropreate name / not self-explaining
 # Define NA threshold to remove columns
 secom.NA.threshold<-20
 
@@ -93,27 +90,25 @@ secom.plot.2<-ggplot(secom.NA.col.table,aes(x=Feature,y=Percentage)) + geom_poin
 secom.plot.2<-secom.plot.2+geom_line(aes(x=Feature, y = secom.NA.threshold), col="red") + geom_line(aes(x=Feature, y= 10), col="darkblue", linetype="dashed")
 secom.plot.2
 
-# !!! good approach but histogram should be added
 # histogram of Percentage of NA per column
 secom.plot.3<-ggplot(secom.NA.col.table) + geom_histogram(aes(x = Percentage), binwidth = 10, boundary = 0, closed = "left", fill="limegreen", col = "black") + scale_x_continuous(breaks = seq(0,100,10)) + theme_bw() + ggtitle("Percentage of NaN per Feature") + theme(plot.title = element_text(hjust = 0.5,face = "bold"),axis.text = element_text(color = "black")) + ylab("Number of features") + xlab("Percentage of NA")
 # plot the NA threshold line
 secom.NA.line<-data.frame(secom.NA.line=secom.NA.threshold)
 secom.plot.3 + geom_vline(data = secom.NA.line, aes(xintercept=secom.NA.line), linetype = "dashed", color="red")
 
-# Looking at the percentage of "NaN" per column, we determined our threshold to __20%__ (red line). In other words, any column that has greater than 20% of "NaN" shall be removed.
+# Looking at the percentage of "NaN" per column, I determined our threshold to __20%__ (red line). In other words, any column that has greater than 20% of "NaN" shall be removed.
 
 # Dataset after removing columns with greater than 20% of NA 
 secom.clean.1<-secom.data.train[, ! secom.na.perc(secom.data.train,2)>secom.NA.threshold]
 
 ## 3. Outliers
 # standardize (z-score) secom
-
 secom.zscore<-data.frame(scale(secom.clean.1))
 
 # count of outliers in each column using 3s rule
 secom.outliers<-apply(secom.zscore,2,function(x) length(which(abs(x)>3)))
 
-# We considered whether we could remove the entire record or not. Hence we looked at the at the percentage of "NaN" per row.
+# I considered whether the entire record could be removed, hence looked at the at the percentage of "NaN" per row.
 # count and percentage of NA per row
 secom.data.pass<-secom.training[secom.training$Status==-1,3:ncol(secom.training)]  # data frame of features of only Pass cases
 secom.NA.per.row.perc <- secom.na.perc(secom.data.pass,1)
@@ -124,44 +119,33 @@ secom.NA.row.table<-data.frame(Row = 1:nrow(secom.training[secom.training$Status
 secom.plot.4<-ggplot(secom.NA.row.table,aes(x=Row,y=Percentage)) + geom_point(aes(color=Frequency), size = 2) + scale_color_continuous(low = "limegreen", high = "dodgerblue4") + theme_bw() + ggtitle("Percentage of NaN per Record") + theme(plot.title = element_text(hjust = 0.5,face = "bold"),axis.text = element_text(size = 10, color = "black"))
 secom.plot.4
 
-# !!! Histogram needed additionally!
 # histogram of Percentage of NA per row
 secom.plot.5<-ggplot(secom.NA.row.table) + geom_histogram(aes(x = Percentage), binwidth = 2, boundary = 0, closed = "left", fill="limegreen", col = "black") + scale_x_continuous(breaks = seq(0,30,2)) + theme_bw() + ylab("Number of features") + xlab("Percentage of NA") + ggtitle("Percentage of NaN per Record") + theme(plot.title = element_text(hjust = 0.5,face = "bold"),axis.text = element_text(color = "black"))
 secom.plot.5
 
-# Note: Features with desired outcome (i.e. Fails or "1") were not removed from the data set. This was factored into our calculation of the threshold mentioned above. 
-# We determined that the percentage of NA per row would be too low to exclude entire records
-# Therefore, we transform the outlier to NA and will impute values to replace outliers in the following stages.
+# The percentage of NA per row would be too low to exclude entire records
+# Therefore, transform the outlier to NA and impute values to replace outliers in the following stages.
 
 # Transform outliers to NA
 secom.trimmed<-secom.clean.1
 secom.trimmed[abs(secom.zscore)>3] = NA
 
-# !!! perfect usage of apply
-# !!! But more appropreate approach would be to focus on vola and define a threshold.
-
 ## 4. Features with low volatility
-
-
-ncol(secom[,apply(secom.data,2,var,na.rm=TRUE) == 0])
-
 # SECOM dataset consists of columns with low volatility, which do not show much difference between Pass/Fail cases 
 # Coefficient of variance of features 
 secom.coef.var<-apply(secom.trimmed,2,cv,na.rm=TRUE)
 
-# Histogram of volatility (The range of absolute coefficient of variance is large but we focus on xlim from 0 to 500, because higher volatility columns will be kept anyways)
+# Histogram of volatility (The range of absolute coefficient of variance is large but focus on xlim from 0 to 500, because higher volatility columns will be kept anyways)
 par(las=1)
 hist(abs(secom.coef.var),breaks=seq(0,50000,5), xlim=c(0,500), col = "limegreen", main = "Histogram of volatility", xlab = "CV", ylab = "Number of features", cex.main=1.3)
 abline(v=5, col = "red", lty = 2)
 
-
-# we decide the volatility threshold to be 5 and will remove the columns with volatility less than threshold
+# volatility threshold = 5 and will remove the columns with volatility less than threshold
 # volatility threshold
 secom.vola.threshold<-5
 # Dataset after removing low-volatility features
 secom.clean.2<-secom.trimmed[,which(abs(secom.coef.var)>secom.vola.threshold)]
 
-# !!! good doc!
 #== Bar chart: change of number of Features
 secom.clean<-c(ncol(secom.data.train),ncol(secom.clean.1),ncol(secom.clean.2))
 secom.plot.6<-barplot(secom.clean, names.arg = c("Original data","After removing \n high NA","After removing \n low volatility"),ylab = "Number of features", col = "limegreen",ylim = c(0,650),main = "Change of Number of Features")
@@ -170,15 +154,11 @@ text(secom.plot.6, y=secom.clean, secom.clean, pos = 3)
 ## 5. Data Scaling/ Transformation
 # The features of SECOM dataset have different unit of measures and their range of values vary widely
 # therefore scaling is necessary especially when some algorithms take the distance between data points into consideration.
-# We try Min-Max scaling with the range of [0,1] and also z-score scaling (standardization) and select Min-Max scaling for next steps
 # Min-Max scaling of range [0,1]
 secom.scaled<-data.frame(apply(secom.clean.2,2,rescale,to=c(0,1)))
 
-# z-score scaling (standardization)
-# secom.zscore<-data.frame(scale(secom.clean.2))
-
 # Algorithms with assumption of normal distribution will require data transformation before building the model. 
-# Hence, we also prepare a Yeo-Johnson transformation function for later step (because SECOM contains zero and negative values).
+# Hence, a Yeo-Johnson transformation function is prepared for later step if necessary (because SECOM contains zero and negative values).
 secom.yj.transform<-function(original.var){
   
   # determine best lambda for Yeo-Johnson
@@ -201,7 +181,7 @@ anyNA(secom.knn.imputed)
 
 ## 7. Feature selection
 ### 7b. Principal Component Analysis (PCA)
-# We performed PCA as a method for feature selection/reduction. 
+# PCA as a method for feature selection/reduction. 
 # Transform variables to components
 secom.pca.comp <- prcomp(secom.knn.imputed)
 # std deviation of each component
@@ -254,7 +234,7 @@ text(secom.plot.7, y=secom.clean, secom.clean, pos = 3)
 
 ## 8. Balancing the dataset
 # Most machine learning classification algorithms are sensitive to imbalance in the classes. An imbalance dataset will bias the predictive model towards the majority class. 
-# Therefore, we need to implement a certain methods to balance the data in order to build an efficient model.
+# Therefore, need to implement a certain methods to balance the data in order to build an efficient model.
 # ROSE
 secom.train.model.rose<-ROSE(Status~.,secom.train.model,seed = 1)$data
 print("Number of each class after ROSE")
@@ -269,7 +249,6 @@ secom.model_rf.rose<-randomForest(Status~.,secom.train.model.rose,importance = T
 print("Random Forest model built on ROSE")
 print(secom.model_rf.rose)
 
-
 ### 9b. Predict on Test set
 # Prepare Test set with features selected in Training set
 secom.test.model<-secom.test[,c("Status", secom.selected.features)]
@@ -279,15 +258,6 @@ secom.test.model.rescaled<-data.frame(Status = secom.test.model[,1],apply(secom.
 
 # remove rows that have NA value to avoid error in next prediction step
 secom.test.model.rescaled<-secom.test.model.rescaled[rowSums(is.na(secom.test.model.rescaled)) == 0,]
-
-# Predict on Test set
-## Using model built on ROSE
-#secom.model_predict.rose<-predict(secom.model_rf.rose,secom.test.model.rescaled,type="prob")
-## data frame of actual Status and predicted Status
-#secom.final.rose<-data.frame(actual = secom.test.model.rescaled$Status%>%as.factor(), predict = secom.model_predict.rose)
-## probability threshold = 0.5
-#secom.final.rose$predict<-ifelse(secom.final.rose$predict.1>0.5, 1, -1)%>%as.factor()
-
 
 ### 9c. Assess the model performance
 # Assess the model performance
